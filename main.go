626d3b9c9d332e858f3fc8e78f211f484d3edfae
tdepuy@xmatters.com <tdepuy@xmatters.com>
2020-06-07 14:18:25 -0700

    Tweaking Static...

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 func main() {
 	r := gin.Default()
     r.Static("/", "./")
-    r.Static("/img", "./img")
+    r.Static("/img/", "./img/")
     r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
